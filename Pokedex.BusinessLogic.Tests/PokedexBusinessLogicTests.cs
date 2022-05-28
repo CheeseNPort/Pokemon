596ea70d0bf4f90e3679bf41e3bab4f2f7d3aae8
mikem <mikem@DESKTOP-N9FQSQH>
2022-05-28 15:48:32 +0100

    Added unit tests for translated pokemons

--- Pokedex.BusinessLogic.Tests/PokedexBusinessLogicTests.cs
+++ Pokedex.BusinessLogic.Tests/PokedexBusinessLogicTests.cs
@@ -1,6 +1,7 @@
 using Moq;
 using Pokedex.Pokemons;
 using Pokedex.Pokemons.Models;
+using Pokedex.Translations;
 using System;
 using System.Threading.Tasks;
 using Xunit;
@@ -30,14 +31,107 @@ namespace Pokedex.BusinessLogic.Tests
         public async Task GetPokemon_ProvidingIncorrectName_ThrowsException()
         {
             // Arrange
-            var pokemons = new Mock<IPokemons>();
             var correctPokemonName = "correctname";
+
+            var pokemons = new Mock<IPokemons>();
             pokemons.Setup(x => x.GetPokemon(It.Is<string>(s => s == correctPokemonName))).ReturnsAsync(new Pokemon(correctPokemonName, "", "", false));
             pokemons.Setup(x => x.GetPokemon(It.IsAny<string>())).Throws<Exception>();
+
             var businessLogic = new PokedexBusinessLogic(pokemons.Object, null);
 
             // Act and Assert
             await Assert.ThrowsAsync<Exception>(() => businessLogic.GetPokemon("someothername"));
+        }
+
+        [Fact]
+        public async Task GetPokemonTranslated_GettingLegendaryPokemon_TranslatesToYoda()
+        {
+            // Arrange
+            var englishDescription = "Description in English";
+            var yodaTranslation = "Description in Yoda";
+
+            var pokemons = new Mock<IPokemons>();
+            pokemons.Setup(x => x.GetPokemon(It.IsAny<string>())).ReturnsAsync(new Pokemon("", englishDescription, "", true));
+
+            var translations = new Mock<ITranslate>();
+            translations.Setup(x => x.Translate(It.Is<string>(s => s == englishDescription), It.Is<TranslationLanguage>(t => t == TranslationLanguage.Yoda))).ReturnsAsync(yodaTranslation);
+
+            var businessLogic = new PokedexBusinessLogic(pokemons.Object, translations.Object);
+
+            // Act
+            var pokemon = await businessLogic.GetPokemonTranslated("");
+
+            // Assert
+            Assert.NotNull(pokemon);
+            Assert.Equal(yodaTranslation, pokemon.Description);
+        }
+
+        [Fact]
+        public async Task GetPokemonTranslated_GettingCavePokemon_TranslatesToYoda()
+        {
+            // Arrange
+            var englishDescription = "Description in English";
+            var yodaTranslation = "Description in Yoda";
+
+            var pokemons = new Mock<IPokemons>();
+            pokemons.Setup(x => x.GetPokemon(It.IsAny<string>())).ReturnsAsync(new Pokemon("", englishDescription, "cave", false));
+
+            var translations = new Mock<ITranslate>();
+            translations.Setup(x => x.Translate(It.Is<string>(s => s == englishDescription), It.Is<TranslationLanguage>(t => t == TranslationLanguage.Yoda))).ReturnsAsync(yodaTranslation);
+
+            var businessLogic = new PokedexBusinessLogic(pokemons.Object, translations.Object);
+
+            // Act
+            var pokemon = await businessLogic.GetPokemonTranslated("");
+
+            // Assert
+            Assert.NotNull(pokemon);
+            Assert.Equal(yodaTranslation, pokemon.Description);
+        }
+
+        [Fact]
+        public async Task GetPokemonTranslated_GettingNonCaveNonLegendaryPokemon_TranslatesToShakespeare()
+        {
+            // Arrange
+            var englishDescription = "Description in English";
+            var shakespeareDescription = "Description in Shakespeare";
+
+            var pokemons = new Mock<IPokemons>();
+            pokemons.Setup(x => x.GetPokemon(It.IsAny<string>())).ReturnsAsync(new Pokemon("", englishDescription, "", false));
+
+            var translations = new Mock<ITranslate>();
+            translations.Setup(x => x.Translate(It.Is<string>(s => s == englishDescription), It.Is<TranslationLanguage>(t => t == TranslationLanguage.Shakespeare))).ReturnsAsync(shakespeareDescription);
+
+            var businessLogic = new PokedexBusinessLogic(pokemons.Object, translations.Object);
+
+            // Act
+            var pokemon = await businessLogic.GetPokemonTranslated("");
+
+            // Assert
+            Assert.NotNull(pokemon);
+            Assert.Equal(shakespeareDescription, pokemon.Description);
+        }
+
+        [Fact]
+        public async Task GetPokemonTranslated_WhenTranslationFails_KeepsDescriptionInEnglish()
+        {
+            // Arrange
+            var englishDescription = "Description in English";
+
+            var pokemons = new Mock<IPokemons>();
+            pokemons.Setup(x => x.GetPokemon(It.IsAny<string>())).ReturnsAsync(new Pokemon("", englishDescription, "", true));
+
+            var translations = new Mock<ITranslate>();
+            translations.Setup(x => x.Translate(It.IsAny<string>(), It.IsAny<TranslationLanguage>())).Throws<Exception>();
+
+            var businessLogic = new PokedexBusinessLogic(pokemons.Object, translations.Object);
+
+            // Act
+            var pokemon = await businessLogic.GetPokemonTranslated("");
+
+            // Assert
+            Assert.NotNull(pokemon);
+            Assert.Equal(englishDescription, pokemon.Description);
         }
     }
 }
